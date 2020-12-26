e6c106d2d9b52a248a32bb1ff342c596a64e6233
Kaushik <kaushikkapoor1996@gmail.com>
2020-12-26 13:38:06 +0530

    Hello World Base

---
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloController.java
@@ -0,0 +1,18 @@
+package com.stacksimplify.restservices.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/helloworld1")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("Kaushik", "Kapoor", "Lucknow");
+	}
+}
